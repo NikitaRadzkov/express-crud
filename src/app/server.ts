711e213c9267bc8b13d60d163243cb96f2f4ca45
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-26 10:43:42 +0300

    Add post controller

--- src/app/server.ts
+++ src/app/server.ts
@@ -2,11 +2,12 @@ import 'dotenv/config';
 import App from './app';
 import UsersController from './users/users.controller';
 import { validateEnv } from '../utils/validate-env';
+import PostsController from './posts/post.controller';
 
 const port = process.env.PORT || 5500;
 
 validateEnv();
 
-const app = new App([new UsersController()], port);
+const app = new App([new UsersController(), new PostsController()], port);
 
 app.listen();
