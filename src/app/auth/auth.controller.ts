30dce5d324ae27fb371ed304dc7cc7826bc1b631
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-26 17:48:19 +0300

    Add jwt token logic

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import SameDataException from '../../exceptions/same-data.exception';
 import Controller from '../../interfaces/controller.interface';
 import usersModel from '../users/user.model';
@@ -7,6 +8,9 @@ import CreateUserDto from './dto/create-user.dto';
 import LoginDto from './dto/login.dto';
 import WrongCredentials from '../../exceptions/wrong-credentials.exception';
 import validationMiddleware from '../../middleware/validation.middleware';
+import User from '../users/user.interface';
+import TokenData from '../../interfaces/token-data.interface';
+import DataStoredInToken from '../../interfaces/data-stored-in-token';
 
 class AuthenticationController implements Controller {
   path = '/auth';
@@ -22,6 +26,20 @@ class AuthenticationController implements Controller {
     this.router.post(`${this.path}/login`, validationMiddleware(LoginDto), this.login);
   }
 
+  private createToken(user: User): TokenData {
+    const expiresIn = 60 * 60; // an hour
+    const secret = process.env.JWT_SECRET;
+    const dataStoredInToken: DataStoredInToken = {
+      _id: user._id,
+    };
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return { expiresIn, token: jwt.sign(dataStoredInToken, secret!, { expiresIn }) };
+  }
+
+  private createCookie(tokenData: TokenData) {
+    return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`;
+  }
+
   private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
     const userData: CreateUserDto = request.body;
     if (await this.user.findOne({ email: userData.email })) {
@@ -30,6 +48,8 @@ class AuthenticationController implements Controller {
       const hashedPassword = await bcrypt.hash(userData.password, 4);
       const user = await this.user.create({ ...userData, password: hashedPassword });
       user.password = undefined;
+      const tokenData = this.createToken(user);
+      response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
       response.send(user);
     }
   };
@@ -42,6 +62,8 @@ class AuthenticationController implements Controller {
       const isPasswordMatching = await bcrypt.compare(loginData.password, user.password!);
       if (isPasswordMatching) {
         user.password = undefined;
+        const tokenData = this.createToken(user);
+        response.setHeader('Set-Cookie', [this.createCookie(tokenData)]);
         response.send(user);
       } else {
         next(new WrongCredentials());
