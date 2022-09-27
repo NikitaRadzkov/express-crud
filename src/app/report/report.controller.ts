005c73921e4f44509b3e515c67a2d3de47ad8b3e
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-27 17:08:22 +0300

    Create report controller, add aggregate route

---
+++ src/app/report/report.controller.ts
@@ -0,0 +1,68 @@
+import * as express from 'express';
+import Controller from '../../interfaces/controller.interface';
+import userModel from '../users/user.model';
+
+class ReportController implements Controller {
+  path = '/report';
+  router = express.Router();
+  private user = userModel;
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.generateReport);
+  }
+
+  private generateReport = async (request: express.Request, response: express.Response) => {
+    const usersByCountries = await this.user.aggregate([
+      {
+        $match: {
+          'address.country': {
+            $exist: true,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            country: '$address.country',
+          },
+          users: {
+            $push: {
+              name: '$name',
+              _id: '$_id',
+            },
+          },
+          count: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'users._id',
+          foreignField: '_id',
+          as: 'users',
+        },
+      },
+      {
+        $addFields: {
+          amountOfArticles: {
+            $size: '$articles',
+          },
+        },
+      },
+      {
+        $sort: {
+          amountOfArticles: 1,
+        },
+      },
+    ]);
+    response.send({ usersByCountries });
+  };
+}
+
+export default ReportController;
